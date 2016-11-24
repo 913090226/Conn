c06e08f12527065a7f648abe2a891c289203d905
jerry <913090226@qq.com>
2016-11-24 18:09:48 +0800

    conn

--- src/main/java/com/jinjiang/dao/Impl/EmpDapImpl.java
+++ src/main/java/com/jinjiang/dao/Impl/EmpDapImpl.java
@@ -16,18 +16,18 @@ import java.util.List;
 public class EmpDapImpl implements IEmpDao {
     private Connection conn;
 
+    public EmpDapImpl() {
+    }
+
+    public EmpDapImpl(Connection conn) {
+        this.conn = conn;
+    }
+
     public Connection getConn() {
         return conn;
     }
 
     public void setConn(Connection conn) {
-        this.conn = conn;
-    }
-
-    public EmpDapImpl() {
-    }
-
-    public EmpDapImpl(Connection conn) {
         this.conn = conn;
     }
 
@@ -70,9 +70,9 @@ public class EmpDapImpl implements IEmpDao {
             String str = "select COUNT(1) as runcount from employee";
             PreparedStatement stm = conn.prepareStatement(str);
             ResultSet result = stm.executeQuery();
-            if(result.next()){
-                return(result.getInt("runcount"));
-            }else{
+            if (result.next()) {
+                return (result.getInt("runcount"));
+            } else {
                 return 0;
             }
 
@@ -82,24 +82,48 @@ public class EmpDapImpl implements IEmpDao {
     }
 
     public boolean doCreate(Employee emp) {
-        try{
+        try {
             String str = "INSERT INTO employee (empno,name,hireday,salary)VALUES(?,?,?,?)";
             PreparedStatement stm = conn.prepareStatement(str);
-            stm.setInt(1,emp.getEmpno());
-            stm.setString(2,emp.getName());
+            stm.setInt(1, emp.getEmpno());
+            stm.setString(2, emp.getName());
             stm.setDate(3, new Date(emp.getHireday().getTime()));
-            stm.setDouble(4,emp.getSalary());
+            stm.setDouble(4, emp.getSalary());
+            return stm.executeUpdate() == 1;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean doUpdate(Employee emp) {
+        try {
+            PreparedStatement stm = conn.prepareStatement("UPDATE employee SET empno = ?,name = ?,hireday = ?,salary = ? WHERE empno = ?");
+            stm.setString(1, emp.getName());
+            stm.setDate(2, new Date(emp.getHireday().getTime()));
+            stm.setDouble(3, 500);
+            stm.setInt(4, emp.getEmpno());
             return stm.executeUpdate() == 1;
         }catch(Exception e){
             return false;
         }
     }
 
-    public boolean doUpdate(Employee emp) {
-        return false;
-    }
-
     public boolean doDalete(List<Integer> empnoList) {
-        return false;
+        try {
+            StringBuffer sqlStr = new StringBuffer();
+            for (Integer i : empnoList) {
+                sqlStr.append("'"+i.toString()+"'");
+                sqlStr.append(",");
+            }
+            if (empnoList.size() > 0) {
+                sqlStr.delete(sqlStr.length() - 1, sqlStr.length());
+                sqlStr = "DELETE FROM employee WHERE empno in (" + sqlStr + ")";
+                return conn.prepareStatement(sqlStr).executeUpdate()==empnoList.size();
+            } else {
+                return false;
+            }
+        }catch(Exception e){
+            return false;
+        }
     }
 }
